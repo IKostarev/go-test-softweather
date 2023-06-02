2201a3fd7db418c43d06bbc65ba33e0bb348f8e1
Ilya Kostarev <ilya.kostarev.9@gmail.com>
2023-06-02 17:12:22 +0500

    feat: add main file

--- cmd/substring/main.go
+++ cmd/substring/main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"go-test-softweather/internal/config"
+	"go-test-softweather/internal/handlers"
+	"log"
+	"net/http"
+)
 
+func main() {
+	cfg := config.LoadConfig()
+
+	fmt.Printf("server start on %s port\n", cfg.ServerAddr)
+
+	http.HandleFunc("/", handlers.HelloHandler)
+	http.HandleFunc("/api/substring", handlers.SubstringHandler)
+
+	log.Fatal(http.ListenAndServe(cfg.ServerAddr, nil))
 }
